f7d72b266af684038194304de338b3a97c556ba4
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-03-25 15:08:44 +0100

    feat: liste_noeud

--- liste_noeud.c
+++ liste_noeud.c
@@ -1,8 +1,156 @@
 #define _GNU_SOURCE
+
 #include "liste_noeud.h"
 #include <stdlib.h>
 #include <math.h>
 
-/* TODO */
+struct _cellule {
+    noeud_id_t noeud;
+    double distance;
+    noeud_id_t precedent;
+    struct _cellule *suivant;
+};
+
+typedef struct _cellule cellule_t;
+
+struct liste_noeud_t {
+    cellule_t *tete;
+    cellule_t *queue;
+};
+
+liste_noeud_t *creer_liste() {
+    liste_noeud_t *liste = malloc(sizeof(liste_noeud_t));
+    liste->tete = NULL;
+    liste->queue = NULL;
+    return liste;
+}
+
+void detruire_liste(liste_noeud_t **liste_ptr) {
+    cellule_t *curseur = (*liste_ptr)->tete;
+    while (curseur != NULL) {
+        cellule_t *tmp = curseur;
+        curseur = curseur->suivant;
+        free(tmp);
+    }
+    free(*liste_ptr);
+    *liste_ptr = NULL;
+}
+
+bool est_vide_liste(const liste_noeud_t *liste) {
+    return liste->tete == NULL;
+}
+
+bool contient_noeud_liste(const liste_noeud_t *liste, noeud_id_t noeud) {
+    cellule_t *curseur = liste->tete;
+
+    while (curseur != NULL && curseur->noeud != noeud) {
+        curseur = curseur->suivant;
+    }
+
+    return curseur != NULL;
+}
+
+bool contient_arrete_liste(const liste_noeud_t *liste, noeud_id_t source, noeud_id_t destination) {
+    cellule_t *curseur = liste->tete;
+
+    while (curseur != NULL && (curseur->noeud != destination || curseur->precedent != source)) {
+        curseur = curseur->suivant;
+    }
+
+    return curseur != NULL;
+}
+
+double distance_noeud_liste(const liste_noeud_t *liste, noeud_id_t noeud) {
+    cellule_t *curseur = liste->tete;
+
+    while (curseur != NULL && curseur->noeud != noeud) {
+        curseur = curseur->suivant;
+    }
+
+    return curseur == NULL ? INFINITY : curseur->distance;
+}
+
+noeud_id_t precedent_noeud_liste(const liste_noeud_t *liste, noeud_id_t noeud) {
+    cellule_t *curseur = liste->tete;
+
+    while (curseur != NULL && curseur->noeud != noeud) {
+        curseur = curseur->suivant;
+    }
+
+    return curseur == NULL ? NO_ID : curseur->precedent;
+}
+
+noeud_id_t min_noeud_liste(const liste_noeud_t *liste) {
+    cellule_t *curseur = liste->tete;
+
+    noeud_id_t id_min = NO_ID;
+    double dist_min = INFINITY;
+
+    while (curseur != NULL) { // parcours de toute la liste chainé
+        if (curseur->distance < dist_min) {
+            id_min = curseur->noeud;
+            dist_min = curseur->distance;
+        }
+        curseur = curseur->suivant;
+    }
+
+    return id_min;
+}
+
+void inserer_noeud_liste(liste_noeud_t* liste, noeud_id_t noeud, noeud_id_t precedent, double distance) {
+    cellule_t *nouvelle_cellule = (cellule_t* )malloc(sizeof(cellule_t));
+    nouvelle_cellule->noeud = noeud;
+    nouvelle_cellule->precedent = precedent;
+    nouvelle_cellule->distance = distance;
+    nouvelle_cellule->suivant = NULL;
+
+    if (liste->queue == NULL) {
+        liste->tete = nouvelle_cellule;
+        liste->queue = nouvelle_cellule;
+    } else {
+        liste->queue->suivant = nouvelle_cellule;
+        liste->queue = nouvelle_cellule;
+    }
+
+}
 
 
+void changer_noeud_liste(liste_noeud_t *liste, noeud_id_t noeud, noeud_id_t precedent, double distance) {
+    cellule_t *curseur = liste->tete;
+
+    while (curseur != NULL && curseur->noeud != noeud) {
+        curseur = curseur->suivant;
+    }
+
+    if (curseur != NULL) {
+        curseur->distance = distance;
+        curseur->precedent = precedent;
+    }
+    else {
+        inserer_noeud_liste(liste, noeud, precedent, distance);
+    }
+}
+
+void supprimer_noeud_liste(liste_noeud_t* liste, noeud_id_t noeud) {
+    cellule_t *curseur = liste->tete;
+    cellule_t *precedent = NULL;
+
+    while (curseur != NULL && curseur->noeud != noeud) {
+        precedent = curseur;
+        curseur = curseur->suivant;
+    }
+
+    if (curseur != NULL) {
+        if (precedent == NULL) {
+            liste->tete = curseur->suivant;
+        } else {
+            precedent->suivant = curseur->suivant;
+        }
+
+        if (curseur == liste->queue) {
+            liste->queue = precedent;
+        }
+
+        free(curseur);
+    }
+}
--- liste_noeud.h
+++ liste_noeud.h
@@ -1,9 +1,12 @@
 #include "graphe.h"
 #include <stdbool.h>
 
-// TODO : type liste_noeud_t
+// type liste_noeud_t
+struct liste_noeud_t;
 
-// TODO : typedef
+// typedef
+typedef struct liste_noeud_t liste_noeud_t;
+
 
 /**
  * creer_liste : crée une liste de noeuds, initialement vide
@@ -11,7 +14,7 @@
  * Post-conditions : `r = creer_liste()` => `r != NULL`, `est_vide_liste(r)`
  * @return liste nouvellement créée (de type liste_noeud_t)
  */
-/* TODO : type de retour */ creer_liste();
+liste_noeud_t* creer_liste();
 
 /**
  * detruire_liste : détruit la liste passée en paramètre
@@ -21,7 +24,7 @@
  *
  * @param liste_ptr pointeur sur la liste à détruire
  */
-void detruire_liste(/* TODO : type paramètre */ liste_ptr);
+void detruire_liste(liste_noeud_t** liste_ptr);
 
 /**
  * est_vide_liste : test si la liste passée en paramètre est vide
@@ -31,7 +34,7 @@ void detruire_liste(/* TODO : type paramètre */ liste_ptr);
  * @param liste [in] liste à tester
  * @return vrai ssi la liste ne contient aucun élément
  */
-// TODO : est_vide_liste
+bool est_vide_liste(const liste_noeud_t* liste);
 
 /**
  * contient_noeud_liste : test si le noeud donné appartient à la liste donnée.
@@ -69,7 +72,7 @@ bool contient_arrete_liste(const liste_noeud_t* liste, noeud_id_t source, noeud_
  * @param noeud noeud dont on veut la distance
  * @return distance associée à noeud dans liste ou INFINITY si noeud n'est pas dans liste
  */
-// TODO : distance_noeud_liste
+double distance_noeud_liste(const liste_noeud_t* liste, noeud_id_t noeud);
 
 /**
  * precedent_noeud_liste : récupère le noeud précédent associé au noeud donné dans la liste donnée.
@@ -82,7 +85,7 @@ bool contient_arrete_liste(const liste_noeud_t* liste, noeud_id_t source, noeud_
  * @param noeud noeud dont on veut le précédent
  * @return précédent associé au noeud dans la liste (ou `NO_ID` si noeud n'est pas dans liste)
  */
-// TODO : precedent_noeud_liste
+noeud_id_t precedent_noeud_liste(const liste_noeud_t* liste, noeud_id_t noeud);
 
 /**
  * min_noeud_liste : trouve le (un) noeud de la liste dont la distance associée est la plus petite,
@@ -95,7 +98,7 @@ bool contient_arrete_liste(const liste_noeud_t* liste, noeud_id_t source, noeud_
  * @param liste [in] liste à parcourir
  * @return noeud qui minimise la distance, ou `NO_ID` si pas de noeud
  */
-// TODO : min_noeud_liste
+noeud_id_t min_noeud_liste(const liste_noeud_t* liste);
 
 /**
  * inserer_noeud_liste : insère le noeud donné dans la liste
@@ -107,7 +110,7 @@ bool contient_arrete_liste(const liste_noeud_t* liste, noeud_id_t source, noeud_
  * @param precedent noeud précédent du noeud à insérer (prec(n))
  * @param distance distance du noeud à insérer (dist(n))
  */
-// TODO : inserer_noeud_liste
+void inserer_noeud_liste(liste_noeud_t* liste, noeud_id_t noeud, noeud_id_t precedent, double distance);
 
 /**
  * changer_noeud_liste : modifie les valeurs associées au noeud donné dans la liste donnée.
@@ -124,7 +127,7 @@ bool contient_arrete_liste(const liste_noeud_t* liste, noeud_id_t source, noeud_
  * @param precedent nouveau noeud précédent pour noeud
  * @param distance nouvelle distance pour noeud
  */
-// TODO : changer_noeud_liste
+void changer_noeud_liste(liste_noeud_t* liste, noeud_id_t noeud, noeud_id_t precedent, double distance);
 
 /**
  * supprimer_noeud_liste : supprime le noeud donné de la liste. Si le noeud n'est pas dans la liste,
@@ -136,6 +139,6 @@ bool contient_arrete_liste(const liste_noeud_t* liste, noeud_id_t source, noeud_
  * @param liste [in,out] liste à modifier
  * @param noeud noeud à supprimer de liste
  */
-// TODO : supprimer_noeud_liste
+void supprimer_noeud_liste(liste_noeud_t* liste, noeud_id_t noeud);
 
 
