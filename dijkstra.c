858071052c0ea62f1ab79a0ce360ca411b6e44e2
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-04-25 15:03:00 +0200

    fix: it works

--- dijkstra.c
+++ dijkstra.c
@@ -19,34 +19,44 @@
  * @param visites [in] liste des noeuds visités créée par l'algorithme de Dijkstra
  * @param noeud noeud vers lequel on veut construire le chemin depuis le départ
  */
-void construire_chemin_vers(liste_noeud_t** chemin, liste_noeud_t* visites, noeud_id_t noeud) {
+void construire_chemin_vers(liste_noeud_t **chemin, liste_noeud_t *visites, noeud_id_t noeud) {
     noeud_id_t np = precedent_noeud_liste(visites, noeud);
     if (np != NO_ID) {
         construire_chemin_vers(chemin, visites, np);
-        inserer_noeud_liste( *chemin, np, precedent_noeud_liste(visites, np), distance_noeud_liste(visites, np));
+        inserer_noeud_liste(*chemin, np, precedent_noeud_liste(visites, np), distance_noeud_liste(visites, np));
     }
 }
 
 
 float dijkstra(
-    const struct graphe_t* graphe, 
-    noeud_id_t source, noeud_id_t destination, 
-    liste_noeud_t** chemin) {
+        const struct graphe_t *graphe,
+        noeud_id_t source, noeud_id_t destination,
+        liste_noeud_t **chemin) {
 
-    liste_noeud_t* aVisiter = creer_liste();
-    liste_noeud_t* visites = creer_liste();
+    liste_noeud_t *aVisiter = creer_liste();
+    liste_noeud_t *visites = creer_liste();
 
-    inserer_noeud_liste(aVisiter, source, NO_ID, 0);
+    inserer_noeud_liste(aVisiter, source, NO_ID, 0.0);
+
     while (!est_vide_liste(aVisiter)) {
         noeud_id_t nc = min_noeud_liste(aVisiter);
-        inserer_noeud_liste(visites, nc, precedent_noeud_liste(aVisiter, nc), distance_noeud_liste(aVisiter, nc));
+
+        inserer_noeud_liste(
+                visites,
+                nc,
+                precedent_noeud_liste(aVisiter, nc),
+                distance_noeud_liste(aVisiter, nc)
+        );
+
         supprimer_noeud_liste(aVisiter, nc);
 
         size_t nb_voisins = nombre_voisins(graphe, nc);
-        noeud_id_t* voisins = (noeud_id_t* ) malloc(nb_voisins);
+        noeud_id_t *voisins = (noeud_id_t *) malloc(nb_voisins * sizeof(noeud_id_t));
         noeuds_voisins(graphe, nc, voisins);
-        for (size_t i =0; i < nb_voisins; i++ ) {
+
+        for (size_t i = 0; i < nb_voisins; i++) {
             noeud_id_t nv = voisins[i];
+
             if (!contient_noeud_liste(visites, nv)) {
                 double d_actu = distance_noeud_liste(visites, nc) + noeud_distance(graphe, nc, nv);
 
@@ -69,7 +79,7 @@ float dijkstra(
 
     }
 
-    float res = (float)  distance_noeud_liste(visites, destination);
+    float res = (float) distance_noeud_liste(visites, destination);
 
     detruire_liste(&aVisiter);
     detruire_liste(&visites);
